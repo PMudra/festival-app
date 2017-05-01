7658f14a6b424dd1ade5d283e0d542c3570e70c6
Philipp Mudra <philipp.mudra@web.de>
2017-05-01 20:06:29 +0200

    Festival app starting again

--- app/src/main/java/com/example/drachim/festivalapp/DashboardActivity.java
+++ app/src/main/java/com/example/drachim/festivalapp/DashboardActivity.java
@@ -1,6 +1,8 @@
 package com.example.drachim.festivalapp;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.view.View;
 
 import com.example.drachim.festivalapp.fragment.DashboardFragment;
 
@@ -9,5 +11,9 @@ public class DashboardActivity extends RootActivity {
     @Override
     protected Fragment CreateFragment() {
         return new DashboardFragment();
+    }
+
+    public void openFestivalDetail(View view) {
+        startActivity(new Intent(this, FestivalActivity.class));
     }
 }
