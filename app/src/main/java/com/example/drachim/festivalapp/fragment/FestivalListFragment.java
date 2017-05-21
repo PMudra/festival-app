2664c7b1f708f9f88c851e59347468245e9c0b21
Philipp Mudra <philipp.mudra@web.de>
2017-05-21 19:06:17 +0200

    #30 Clean up Festival list fragment and create async festival list loader

--- app/src/main/java/com/example/drachim/festivalapp/fragment/FestivalListFragment.java
+++ app/src/main/java/com/example/drachim/festivalapp/fragment/FestivalListFragment.java
@@ -3,8 +3,11 @@ package com.example.drachim.festivalapp.fragment;
 import android.app.Activity;
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Loader;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,50 +20,24 @@ import android.view.ViewGroup;
 import com.example.drachim.festivalapp.R;
 import com.example.drachim.festivalapp.data.Festival;
 import com.example.drachim.festivalapp.data.FestivalRecyclerViewAdapter;
+import com.example.drachim.festivalapp.data.sqlite.AsyncFestivalLoader;
 import com.example.drachim.festivalapp.data.sqlite.FestivalPlannerDbHelper;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
-public class FestivalListFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
+public class FestivalListFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Festival>>, SwipeRefreshLayout.OnRefreshListener {
+
+    private OnListFragmentInteractionListener onListFragmentInteractionListener;
     private SwipeRefreshLayout swipeRefreshLayout;
     private FestivalPlannerDbHelper festivalPlannerDbHelper;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public FestivalListFragment() {
-    }
-
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static FestivalListFragment newInstance(int columnCount) {
-        FestivalListFragment fragment = new FestivalListFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RecyclerView recyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -83,20 +60,22 @@ public class FestivalListFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        onRefresh();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_festival_list, container, false);
 
-        RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.list);
+        recyclerView = (RecyclerView)view.findViewById(R.id.list);
         festivalPlannerDbHelper = new FestivalPlannerDbHelper(getActivity());
-        recyclerView.setAdapter(new FestivalRecyclerViewAdapter(festivalPlannerDbHelper.ReadFestivals(), mListener));
+        recyclerView.setAdapter(new FestivalRecyclerViewAdapter(new ArrayList<Festival>(), onListFragmentInteractionListener));
 
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                // TODO: Refresh implementieren
-            }
-        });
+        swipeRefreshLayout.setOnRefreshListener(this);
 
         return view;
     }
@@ -114,7 +93,7 @@ public class FestivalListFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
+            onListFragmentInteractionListener = (OnListFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener");
         }
@@ -128,7 +107,7 @@ public class FestivalListFragment extends Fragment {
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         if (activity instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) activity;
+            onListFragmentInteractionListener = (OnListFragmentInteractionListener) activity;
         } else {
             throw new RuntimeException(activity.toString() + " must implement OnListFragmentInteractionListener");
         }
@@ -137,7 +116,7 @@ public class FestivalListFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        onListFragmentInteractionListener = null;
     }
 
     @Override
@@ -146,6 +125,30 @@ public class FestivalListFragment extends Fragment {
         if (festivalPlannerDbHelper != null) {
             festivalPlannerDbHelper.close();
         }
+    }
+
+    @Override
+    public Loader<List<Festival>> onCreateLoader(int id, Bundle args) {
+        return new AsyncFestivalLoader(getActivity(), festivalPlannerDbHelper);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Festival>> loader, List<Festival> data) {
+        recyclerView.setAdapter(new FestivalRecyclerViewAdapter(data, onListFragmentInteractionListener));
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Festival>> loader) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+        if (!swipeRefreshLayout.isRefreshing()) {
+            swipeRefreshLayout.setRefreshing(true);
+        }
+        getLoaderManager().initLoader(0, null, this);
     }
 
     public interface OnListFragmentInteractionListener {
